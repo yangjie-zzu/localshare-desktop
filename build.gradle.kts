808067a5b8dc1baf00edde747171dcc3693de660
yangjie-zzu <1342986150@qq.com>
2025-12-08 18:12:13 +0800

    修改名称

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,8 +46,8 @@ compose.desktop {
         }
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Exe, TargetFormat.Deb)
-            packageName = "localshare-desktop"
-            packageVersion = "1.0.11"
+            packageName = "LocalShare"
+            packageVersion = "1.0.12"
             modules("java.instrument", "java.management", "java.sql", "jdk.unsupported", "java.naming", "jdk.charsets", "jdk.localedata")
             windows {
                 shortcut = true
